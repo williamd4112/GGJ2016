8b1e29aa907d0e1c7760b33224d6593d63f1a4b7
Po-Hsun Chen <godpopo34@gmail.com>
2016-01-30 21:37:01 +0800

    no message

---
+++ Assets/MagicCircle/BulletDam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDam : MonoBehaviour {
+	[SerializeField]
+	private int DamageAmount = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.GetComponent<Damageable> ()) {
+			other.gameObject.GetComponent<Damageable> ().OnHit (DamageAmount);
+		}
+	}
+}
