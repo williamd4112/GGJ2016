f76c94513cefe5bebd4cf5e8420d88598d6399dd
wasdx777 <wasdx777@gmail.com>
2016-01-30 17:25:49 +0800

    canvas hearts

---
+++ Assets/Script/Hearts.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hearts : MonoBehaviour {
+
+	public static Hearts Instance;
+	public GameObject[] heart;
+
+	int index;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < heart.Length; i++) {
+			heart [i].SetActive (true);
+		}
+		index = 4;
+	}
+
+	void Awake(){
+		Instance = this;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Dmg(){
+		heart [index].SetActive (false);
+		index--;
+	}
+}
