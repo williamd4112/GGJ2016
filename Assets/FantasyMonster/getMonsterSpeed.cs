fdf878519d00d3ed5d321fcb8c66ee4a5e5f8ece
Po-Hsun Chen <godpopo34@gmail.com>
2016-01-31 10:11:10 +0800

    no message

---
+++ Assets/FantasyMonster/getMonsterSpeed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getMonsterSpeed : MonoBehaviour {
+	[SerializeField]
+	private Vector3 monsterSpeed;
+	[SerializeField]
+	private float trueSpeed;
+	[SerializeField]
+	private NavMeshAgent m_Nav;
+	[SerializeField]
+	private Animator m_ani;
+	// Use this for initialization
+	void Start () {
+		m_Nav = this.GetComponent<NavMeshAgent> ();
+		m_ani = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		monsterSpeed = m_Nav.velocity;
+		trueSpeed = monsterSpeed.magnitude;
+		m_ani.SetFloat ("Speed", trueSpeed);
+
+	}
+}
